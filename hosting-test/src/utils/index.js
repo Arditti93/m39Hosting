0e05e7dd50b549f63b2663b707d974b985004983
Arditti93 <alex.arditti@hotmail.com>
2022-08-12 13:03:31 +0100

    added API url'

--- hosting-test/src/utils/index.js
+++ hosting-test/src/utils/index.js
@@ -1,8 +1,7 @@
 
 export const Users = async (setter) => {
-    console.log(process.env.REACT_APP_GET_ALL_USERS)
     try {
-        const response = await fetch(process.env.REACT_APP_GET_ALL_USERS,{
+        const response = await fetch(`${process.env.REACT_APP_REST_API}user`,{
             method: "GET",
             headers: {"Content-Type": "application/json"},
         });
